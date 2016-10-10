abcb5ae0edaf820e6d35d2340f693e9616e491fe
Tefa95 <tefadu95@gmail.com>
2016-10-10 11:00:12 +0200

    - Ajout d'un nouveaux layout -envoie des info de co sur le nouveaux layout

--- app/src/main/java/com/example/tefa/projetmaheu/PagePrincipale.java
+++ app/src/main/java/com/example/tefa/projetmaheu/PagePrincipale.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 /**
  * Created by Tefa on 07/10/2016.
@@ -18,15 +19,20 @@ public class PagePrincipale extends AppCompatActivity{
         Bundle extras  = this.getIntent().getExtras();
 
         int Id = extras.getInt("id");
-        String Identifiant = extras.getString("Identifiant");
-        int Xp = extras.getInt("xp");
-        int Quest_fini =extras.getInt("quest_fini");
+        String identifiant = extras.getString("identifiant");
+        int xp = extras.getInt("xp");
+        int quest_fini =extras.getInt("quest_fini");
         Log.e("log_tag", "Id: " + Id +
-                ", Identifiant: " + Identifiant + ", Xp:" + Xp
-                + ", Quest_fini:" +Quest_fini
+                ", Identifiant: " + identifiant + ", Xp:" + xp
+                + ", Quest_fini:" +quest_fini
         );
 
-
+        TextView textViewPseudo = (TextView) findViewById(R.id.pseudo);
+        textViewPseudo.setText(identifiant);
+        TextView textViewXp = (TextView) findViewById(R.id.xp);
+        textViewXp.setText("Expérience: "+String.valueOf(xp));
+        TextView textViewQFini = (TextView) findViewById(R.id.qFinis);
+        textViewQFini.setText("Quêtes finis: "+String.valueOf(quest_fini));
         }
 
 }
